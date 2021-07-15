48966a60647d3a027f3ffb315c16ebd2bcc15d04
pydevnepal <rajukhatri054@gmail.com>
2021-07-15 13:27:34 +0545

    complete task5

---
+++ task5.py
@@ -0,0 +1,7 @@
+# The provided code stub reads and integer, n , from STDIN. For all non-negative integers  i < n, print i^2.
+# Author: Amit Khatri
+
+if __name__ == "__main__":
+    n = int(input())
+    for a in range(n):
+        print(a ** 2)
