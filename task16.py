15c715ba5ddc4f4da004552747fcf2e8a6264511
pydevnepal <rajukhatri054@gmail.com>
2021-07-16 13:10:35 +0545

    contex manager

---
+++ task16.py
@@ -0,0 +1,14 @@
+import csv
+import pandas as pd
+
+# path = "test.csv"
+"""
+a,1,2,3
+b,11,33,33
+c,111,222,333
+"""
+# with open("test.csv", "r") as csvfile:
+#     csv_read = csv.reader(csvfile, delimiter=",")
+#     for row in csv_read:
+#         print(row)
+# print(type(row))
