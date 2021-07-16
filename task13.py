57b4224eb5c59bcdc78f54d07f0011c27675894d
pydevnepal <rajukhatri054@gmail.com>
2021-07-16 13:08:16 +0545

    f string use

---
+++ task13.py
@@ -0,0 +1,11 @@
+def print_full_name(first, last):
+    # Write your code here
+    intro = f"Hello {first} {last}! You just delved into python"
+    print(intro)
+    return intro
+
+
+if __name__ == "__main__":
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
